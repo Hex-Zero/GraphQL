24cb1793bf532ee9f40265a235c9bbaa55aabc76
Hex-Zero <hex0@live.com>
2019-09-10 20:34:17 +0100

    streamline

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `TypeScript`,
-    description: `Typescript practice with Gatsby`,
+    title: `GraphQL`,
+    description: `GraphQL practice with Gatsby`,
     author: `Hex-Zero`,
   },
   plugins: [
@@ -27,11 +27,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
