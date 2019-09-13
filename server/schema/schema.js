e890c0ddbda569d3078a3f451e32eae7d61762fc
Hex-Zero <hex0@live.com>
2019-09-13 07:53:26 +0100

    add graphQL schema

---
+++ server/schema/schema.js
@@ -0,0 +1,34 @@
+const graphql = require("graphql")
+const _ = require("lodash")
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+
+var books = [
+  { name: "Name of the wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+]
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return _.find(books, { id: args.id })
+      },
+    },
+  },
+})
+
+module.exports = new GraphQLSchema({ query: RootQuery })
